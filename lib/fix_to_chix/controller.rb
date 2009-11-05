b924c1d4faeb21a4c9eb6ef11e96b05798b7de19
caike souza <carloshrsouza@gmail.com>
2009-11-05 16:57:00 -0500

    parsing matching options

--- lib/fix_to_chix/controller.rb
+++ lib/fix_to_chix/controller.rb
@@ -4,16 +4,27 @@ require 'fix_to_chix/fixture_parser'
 
 module FixToChix
   class Controller
-    def self.parse_it_all!(options={})
-      matching_option = options[:matching] ? { :matching => Regexp.new(options[:matching]) } : {}
-      selector = FixtureSelector.new(matching_option)
-      %w[spec test].each do |type| 
-        selector.send("#{type}_fixtures").each do |fixture_file_name|
-          parser = FixtureParser.new(fixture_file_name)
-          parser.parse_fixture
-          FactoryWriter.write(parser.output_buffer, FixToChix.class_eval("#{type.upcase}_TARGET_FILE"))
+    
+    class << self
+    
+      def parse_it_all!(options)
+        matching_options = parse_matching_options(options)
+        selector = FixtureSelector.new(matching_options)
+        %w[spec test].each do |type| 
+          selector.send("#{type}_fixtures").each do |fixture_file_name|
+            parser = FixtureParser.new(fixture_file_name)
+            parser.parse_fixture
+            FactoryWriter.write(parser.output_buffer, FixToChix.class_eval("#{type.upcase}_TARGET_FILE"))
+          end
         end
       end
+      
+      def parse_matching_options(options)
+        return {} unless options[:matching]
+        { :matching => Regexp.new(options[:matching]) }
+      end
+    
     end
+  
   end
 end
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,21 +1,45 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe FixToChix::Controller do
-
-  it "parses spec fixtures" do
-    selector = mock('FixtureSelector', :spec_fixtures => ["spec_fix"], :test_fixtures => [])
-    FixToChix::FixtureSelector.stub!(:new).and_return(selector)
-    FixToChix::FixtureParser.should_receive(:new).with("spec_fix").and_return(mock('selector', :parse_fixture => "", :output_buffer => "buff"))
-    FixToChix::FactoryWriter.should_receive(:write).with("buff", FixToChix::SPEC_TARGET_FILE)
-    FixToChix::Controller.parse_it_all!
+  
+  describe "parses spec fixtures" do
+    
+    it "parses spec fixtures" do
+      selector = mock('FixtureSelector', :spec_fixtures => ["spec_fix"], :test_fixtures => [])
+      FixToChix::FixtureSelector.stub!(:new).and_return(selector)
+      FixToChix::FixtureParser.should_receive(:new).with("spec_fix").and_return(mock('Parser', :parse_fixture => "", :output_buffer => "buff"))
+      FixToChix::FactoryWriter.stub!(:write).with("buff", FixToChix::SPEC_TARGET_FILE)
+      FixToChix::Controller.parse_it_all!({})
+    end
+    
+    it "writes to buffer" do
+      selector = mock('FixtureSelector', :spec_fixtures => ["spec_fix"], :test_fixtures => [])
+      FixToChix::FixtureSelector.stub!(:new).and_return(selector)
+      FixToChix::FixtureParser.stub!(:new).with("spec_fix").and_return(mock('Parser', :parse_fixture => "", :output_buffer => "buff"))
+      FixToChix::FactoryWriter.should_receive(:write).with("buff", FixToChix::SPEC_TARGET_FILE)
+      FixToChix::Controller.parse_it_all!({})
+    end
+    
   end
-
-  it "parses test fixtures" do
-    selector = mock('FixtureSelector', :spec_fixtures => [], :test_fixtures => ["test_fix"])
-    FixToChix::FixtureSelector.stub!(:new).and_return(selector)
-    FixToChix::FixtureParser.should_receive(:new).with("test_fix").and_return(mock('selector', :parse_fixture => "", :output_buffer => "buff"))
-    FixToChix::FactoryWriter.should_receive(:write).with("buff", FixToChix::TEST_TARGET_FILE)
-    FixToChix::Controller.parse_it_all!
+  
+  describe "parses test fixtures" do
+    
+    it "parses test fixtures" do
+      selector = mock('FixtureSelector', :spec_fixtures => [], :test_fixtures => ["test_fix"])
+      FixToChix::FixtureSelector.stub!(:new).and_return(selector)
+      FixToChix::FixtureParser.should_receive(:new).with("test_fix").and_return(mock('Parser', :parse_fixture => "", :output_buffer => "buff"))
+      FixToChix::FactoryWriter.stub!(:write).with("buff", FixToChix::TEST_TARGET_FILE)
+      FixToChix::Controller.parse_it_all!({})
+    end
+    
+    it "writes to buffer" do
+      selector = mock('FixtureSelector', :spec_fixtures => [], :test_fixtures => ["test_fix"])
+      FixToChix::FixtureSelector.stub!(:new).and_return(selector)
+      FixToChix::FixtureParser.stub!(:new).with("test_fix").and_return(mock('Parser', :parse_fixture => "", :output_buffer => "buff"))
+      FixToChix::FactoryWriter.should_receive(:write).with("buff", FixToChix::TEST_TARGET_FILE)
+      FixToChix::Controller.parse_it_all!({})
+    end
+    
   end
 
   it "accepts matching one file options for selector" do
@@ -28,9 +52,24 @@ describe FixToChix::Controller do
     FixToChix::Controller.parse_it_all!(:matching => "anakin|yoda")
   end
 
-  it "receives empty hash if no arg" do
+  it "receives empty hash if no arguments for matching" do
     FixToChix::FixtureSelector.should_receive(:new).with({}).and_return(mock('Selector', :spec_fixtures => [], :test_fixtures => []))
-    FixToChix::Controller.parse_it_all!
+    FixToChix::Controller.parse_it_all!({:bla => "bla"})
   end
-
+  
+  it "parses hash from matching arguments if given one" do
+    matching_options = FixToChix::Controller.parse_matching_options(:matching => "blablabla")
+    matching_options.should == { :matching => /blablabla/ }
+  end
+  
+  it "parses hash from matching arguments if given two" do
+    matching_options = FixToChix::Controller.parse_matching_options(:matching => "blablabla|ble")
+    matching_options.should == { :matching => /blablabla|ble/ }
+  end
+  
+  it "parses empty hash from matching arguments if none given" do
+    matching_options = FixToChix::Controller.parse_matching_options({})
+    matching_options.should == {}
+  end
+  
 end
